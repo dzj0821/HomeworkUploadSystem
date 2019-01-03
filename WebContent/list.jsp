722629d73983ddfc0721011c86b3cfb36c8b216d
dzj0821 <dzj0821@126.com>
2019-01-03 18:30:45 +0800

    将List转为JSP

---
+++ WebContent/list.jsp
@@ -0,0 +1,89 @@
+<%@page import="pers.dzj0821.hus.vo.UserClassInfo"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+UserClassInfo[] userClassInfos = request.getAttribute("userClassInfos");
+%>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <%@ include file = "hidden/header_head.jsp" %>
+        <title>作业上传系统-作业列表</title>
+        <style>
+            table{
+                text-align: center;
+            }
+            th{
+                text-align: center;
+            }
+        </style>
+        <script>
+            function del(id) {
+                if (confirm("你确定要删除这个提交吗？")) {
+                    window.location.href = "CancelUploadHomeworkRequest?id=" + id;
+                }
+            }
+        </script>
+    </head>
+
+    <body>
+        <%@ include file = "hidden/header_body.jsp" %>
+        <div class="main" style="text-align: center;">
+            <h1 style="text-align:center;">作业列表</h1>
+
+            <div class="table-responsive">
+                <table class="table table-striped" style="margin:0 auto;">
+                    <thead>
+                        <tr>
+                            <th>作业名称</th>
+                            <th>发布者</th>
+                            <th>状态</th>
+                            <th>操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <% if (userClassInfos != null) {
+                    	for (UserClassInfo userClassInfo : userClassInfos) { %>
+                        	<tr>
+                                <td>${userClassInfo.getHomeworkName() }</td>
+                                <td>${userClassInfo.getPublisherName() }</td>
+                                <td>${userClassInfo.getUploadId() != null ? "已提交" : "未提交" } </td>
+                                <td><% if (userClassInfo.getUploadId() == null) { %>
+                                    	<a href="UploadHomework?id=${userClassInfo.getUploadId() }">提交作业</a>
+                                    <% } else { %>
+                                        <a href="javascript:void(0)" onclick="del(${userClassInfo.getUploadId() })">删除提交</a>
+                                    <% } %>
+                                </td>
+                            </tr>
+                        <% }
+					} %>
+                    </tbody>
+                </table>
+            </div>
+            <% 
+            //分页
+            /*
+              <div style="text-align: center;">
+              <ul class="pagination">
+              <li><a href='./classlist.php?page=1'>&lt;&lt;</a></li>
+              <?php
+              for ($i = $page - 2; $i <= $page + 2; $i++) {
+              if ($i >= 1 && $i <= $max_page) {
+              if ($i == $page) {
+              echo "<li class='active'><a href='./classlist.php?page=$i'>$i</a></li>";
+              } else {
+              echo "<li><a href='./classlist.php?page=$i'>$i</a></li>";
+              }
+              }
+              }
+              ?>
+              <li><a href='./classlist.php?page=<?php echo $max_page; ?>'>&gt;&gt;</a></li>
+              </ul>
+              </div>
+             * 
+             */
+            %>
+    </div>
+</body>
+
+</html>
---
+++ src/pers/dzj0821/hus/servlet/List.java
@@ -0,0 +1,112 @@
+package pers.dzj0821.hus.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pers.dzj0821.hus.dao.HomeworkDao;
+import pers.dzj0821.hus.dao.UploadDao;
+import pers.dzj0821.hus.dao.UserDao;
+import pers.dzj0821.hus.vo.Homework;
+import pers.dzj0821.hus.vo.Upload;
+import pers.dzj0821.hus.vo.User;
+import pers.dzj0821.hus.vo.UserClassInfo;
+
+/**
+ * Servlet implementation class List
+ */
+@WebServlet("/List")
+public class List extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public List() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Integer account = (Integer)session.getAttribute("account");
+		//如果未登录返回主页面
+		if(account == null) {
+			response.sendRedirect("index.jsp");
+			return;
+		}
+		Integer classId = (Integer)session.getAttribute("class_id");
+		//如果session中未存储用户所属的class_id则获取
+		if(classId == null) {
+			UserDao dao = new UserDao();
+			User user = null;
+			try {
+				user = dao.getUser(account);
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+				return;
+			}
+			if(user == null) {
+				response.sendRedirect("index.jsp");
+				return;
+			}
+			classId = user.getClassId();
+		}
+		//获取用户所在班级的作业列表
+		HomeworkDao homeworkDao = new HomeworkDao();
+		Homework[] homeworks = null;
+		try {
+			homeworks = homeworkDao.getHomework(classId);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			return;
+		}
+		//构建用于JSP显示的班级列表数组
+		UserClassInfo[] userClassInfos = new UserClassInfo[homeworks.length];
+		UploadDao uploadDao = new UploadDao();
+		UserDao userDao = new UserDao();
+		for(int i = 0; i < homeworks.length; i++) {
+			int homeworkId = homeworks[i].getId();
+			userClassInfos[i].setHomeworkId(homeworkId);
+			userClassInfos[i].setHomeworkName(homeworks[i].getHomeworkName());
+			//对每个作业查找用户是否已经提交过
+			Upload[] uploads = null;
+			try {
+				uploads = uploadDao.getUpload(account, homeworkId);
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+				return;
+			}
+			userClassInfos[i].setUploadId(uploads.length == 0 ? null : uploads[0].getId());
+			//对每个作业查找发布者名字
+			User user = null;
+			try {
+				user = userDao.getUser(homeworks[i].getPublisherAccount());
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+				return;
+			}
+			userClassInfos[i].setPublisherName(user.getUserName());
+		}
+		request.setAttribute("userClassInfos", userClassInfos);
+		request.getRequestDispatcher("list.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
