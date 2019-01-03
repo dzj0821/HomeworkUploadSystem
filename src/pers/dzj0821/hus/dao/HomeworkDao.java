f6aa79aad03c008ad7d2c1479285dfa96ebc3e93
dzj0821 <dzj0821@126.com>
2019-01-03 18:29:48 +0800

    增加homework表的数据库操作

---
+++ src/pers/dzj0821/hus/dao/HomeworkDao.java
@@ -0,0 +1,33 @@
+package pers.dzj0821.hus.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import pers.dzj0821.hus.vo.Homework;
+
+public class HomeworkDao extends Dao {
+	/**
+	 * 获取指定班级的作业列表
+	 * @param classId 需要获取作业列表的班级id
+	 * @return 存储该班级作业的数组，不会返回NULL
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public Homework[] getHomework(int classId) throws ClassNotFoundException, SQLException {
+		Connection connection = getConnection();
+		PreparedStatement statement = connection.prepareStatement("SELECT * FROM homework WHERE class_id = ?");
+		statement.setInt(1, classId);
+		ResultSet set = statement.executeQuery();
+		Homework[] homeworks = new Homework[getRows(set)];
+		for(int i = 0; i < homeworks.length; i++) {
+			homeworks[i] = new Homework(set);
+			set.next();
+		}
+		set.close();
+		statement.close();
+		connection.close();
+		return homeworks;
+	}
+}
