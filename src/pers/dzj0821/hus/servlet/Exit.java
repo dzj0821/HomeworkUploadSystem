5584eca4d831be8fb6ba2ba5f096656be5fd6507
dzj0821 <dzj0821@126.com>
2019-01-03 14:53:49 +0800

    退出转为JSP

---
+++ src/pers/dzj0821/hus/servlet/Exit.java
@@ -0,0 +1,51 @@
+package pers.dzj0821.hus.servlet;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Exit
+ */
+@WebServlet("/Exit")
+public class Exit extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Exit() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("account") != null) {
+			Enumeration<String> sessionAttrNames = session.getAttributeNames();
+			while(sessionAttrNames.hasMoreElements()) {
+				String name = sessionAttrNames.nextElement();
+				session.removeAttribute(name);
+			}
+		}
+		response.sendRedirect("index.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
