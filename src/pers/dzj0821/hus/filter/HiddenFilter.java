78ff31677f3b6e2810cadd7b62dd6fb0333fc834
dzj0821 <dzj0821@126.com>
2019-01-07 14:25:21 +0800

    增加hidden路径的权限控制

---
+++ src/pers/dzj0821/hus/filter/HiddenFilter.java
@@ -0,0 +1,54 @@
+package pers.dzj0821.hus.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class HiddenFilter
+ */
+@WebFilter("/hidden/*")
+public class HiddenFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public HiddenFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if(request instanceof HttpServletRequest && "administrator".equals(((HttpServletRequest)request).getSession().getAttribute("permission"))) {
+			chain.doFilter(request, response);
+			return;
+		}
+		if(response instanceof HttpServletResponse) {
+			((HttpServletResponse)response).sendError(403);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
