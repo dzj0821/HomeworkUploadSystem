182501af0a786a90d9577e45f0a991680c24ef8b
dzj0821 <dzj0821@126.com>
2019-01-08 23:43:43 +0800

    更新MD5算法与PHP相同

--- src/pers/dzj0821/hus/util/Util.java
+++ src/pers/dzj0821/hus/util/Util.java
@@ -5,28 +5,30 @@ import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 
 public class Util {
-	private static String toHex(byte[] bytes) {
 
-	    final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
-	    StringBuilder ret = new StringBuilder(bytes.length * 2);
-	    for (int i=0; i<bytes.length; i++) {
-	        ret.append(HEX_DIGITS[(bytes[i] >> 4) & 0x0f]);
-	        ret.append(HEX_DIGITS[bytes[i] & 0x0f]);
-	    }
-	    return ret.toString();
-	}
-	
 	public static String MD5(String s) {
-	    try {
-	        MessageDigest md = MessageDigest.getInstance("MD5");
-	        byte[] bytes = md.digest(s.getBytes("utf-8"));
-	        return toHex(bytes);
-	    }
-	    catch (Exception e) {
-	        throw new RuntimeException(e);
-	    }
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		try {
+			byte[] strTemp = s.getBytes();
+			// 使用MD5创建MessageDigest对象
+			MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+			mdTemp.update(strTemp);
+			byte[] md = mdTemp.digest();
+			int j = md.length;
+			char str[] = new char[j * 2];
+			int k = 0;
+			for (int i = 0; i < j; i++) {
+				byte b = md[i];
+				// 将没个数(int)b进行双字节加密
+				str[k++] = hexDigits[b >> 4 & 0xf];
+				str[k++] = hexDigits[b & 0xf];
+			}
+			return new String(str);
+		} catch (Exception e) {
+			return null;
+		}
 	}
-	
+
 	public static String parseUTF8(String string) {
 		try {
 			return new String(string.getBytes("ISO-8859-1"), "UTF-8");
@@ -35,7 +37,7 @@ public class Util {
 		}
 		return null;
 	}
-	
+
 	public static SimpleDateFormat getDateFormater() {
 		return new SimpleDateFormat("yy-MM-dd HH:mm:ss");
 	}
